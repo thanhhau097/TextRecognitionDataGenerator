64ad6f40a889ea96aeb4ce92aba0e1127ea0867e
Lionel <thanhhau097@gmail.com>
2021-01-22 13:29:03 +0000

    fix generator

--- trdg/generators/from_text_file.py
+++ trdg/generators/from_text_file.py
@@ -41,7 +41,7 @@ class GeneratorFromTextFile:
         self.files = [os.path.join(folder, f) for f in os.listdir(folder)]
         self.index = 0
         self.generator = GeneratorFromStrings(
-            strcreate_strings_from_file(self.files[self.index], maximum_length),
+            create_strings_from_file(self.files[self.index], maximum_length),
             count=count,
             fonts=fonts if len(fonts) else load_fonts(language),
             language=language,
