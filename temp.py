fd4eabe610b2264550a081254cfe5c320edfdbbb
Lionel <thanhhau097@gmail.com>
2021-01-22 13:32:21 +0000

    update example file

--- temp.py
+++ temp.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     generator = GeneratorFromTextFile(folder='./trdg/dicts/', count=-1, size=(30, 70), language='en',
                                       minimum_length=1, maximum_length=10,
                                       skewing_angle=2, random_skew=True, blur=1, random_blur=True,
-                                      background_type=3, distorsion_type=-1, margins=(2, 2, 2, 2))
+                                      background_type=0, distorsion_type=-1, margins=(2, 2, 2, 2))
 
     print('Start generating ...')
     for i, (img, lbl) in enumerate(generator):
